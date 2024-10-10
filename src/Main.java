02e2c80e25b502c8f6ea26d94ae1892ed18c7330
stan4 <email@emial.com>
2024-10-10 16:54:31 +0200

    zmiana branch2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Hello and welcome!1111");
+        System.out.println("Hello and welcome!2222");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
